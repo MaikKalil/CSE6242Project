1504a07fb9bd27e8685f2f6262398eb384e92350
Michael <mkjones@gatech.edu>
2023-04-08 10:02:57 -0400

    Region 1 completion

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 import sqlite3
 import pandas as pd
 import numpy as np
@@ -20,10 +21,11 @@ def login():
    email = request.form.get('email')
    if email and '@' in email:
        text_default = ''
-       default_selected = {'zip_pref': '0', 'degree_pref': '0'}
+       default_selected = {'zip_pref': '10', 'degree_pref': '10'}
        print('Request for landing page received with email=%s' % email)
        username = email.split('@')[0]  # split the email at "@" and return only the first part
        user_id = int(hashlib.sha256(email.lower().encode('utf-8')).hexdigest(), 16) % 10**8 # generate a unique numeric ID
+       session['user'] = username
        #Log email to user_id relationship to user table to later identify user rankings
        with sqlite3.connect(db_path) as conn:
            cur = conn.cursor()
@@ -35,20 +37,22 @@ def login():
        print('Request for landing page received with no or invalid email -- redirecting')
        return redirect(url_for('index'))
 
-@app.route('/landing/update', methods=['POST'])
+@app.route('/landing/update', methods=['GET', 'POST'])
 #Helper function to calculate distance between universities and a given zip code.
 # def inst_dist (lat,lon, inst_tbl):
 #     idx = inst_tbl.apply(lambda x: great_circle((x["LATITUDE"], x["LONGITUDE"]), (lat, lon)).miles, axis=1)
 #     return inst_tbl.loc[:, ['UNITID']].assign(DISTANCE_MI=idx)
 def update():
     default_value = 'False'
+    user = session.get('user')
     input_zip = request.form.get('zip')
     max_dist = request.form.get('zip_dist')
+    degree = request.form.get('degree')
     default_selected = {'zip_pref': request.form.get('zip_pref'),
                         'degree_pref': request.form.get('degree_pref')}
-    degree = request.form.get('degree')
+
     print(input_zip, max_dist, degree)
-    return render_template('landing.html', default_value =default_value, default_selected= default_selected,
+    return render_template('landing.html', user =user, default_value =default_value, default_selected= default_selected,
                            zip =input_zip, zip_dist=max_dist,
                            degree= degree)
     # with sqlite3.connect(db_path) as conn:
