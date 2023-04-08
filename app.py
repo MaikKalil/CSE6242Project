c1b460ce70c00e5675c72d1b00a60d875fb4825d
Michael <mkjones@gatech.edu>
2023-04-08 16:02:31 -0400

    formating on region 2. Added states dropdown with multiple selections

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def login():
    email = request.form.get('email')
    if email and '@' in email:
        text_default = ''
-       default_selected = {'zip_pref': '10', 'degree_pref': '10'}
+       default_selected = {'states': ['','GA'], 'zip_pref': '10', 'degree':'3', 'degree_pref': '10'}
        print('Request for landing page received with email=%s' % email)
        username = email.split('@')[0]  # split the email at "@" and return only the first part
        user_id = int(hashlib.sha256(email.lower().encode('utf-8')).hexdigest(), 16) % 10**8 # generate a unique numeric ID
@@ -31,8 +31,12 @@ def login():
            cur = conn.cursor()
            if pd.read_sql(f"SELECT * FROM user WHERE user_id ={user_id}", conn).empty:
                cur.execute(f"INSERT INTO user values('{email}',{user_id})")
+           states = list(pd.read_sql("SELECT DISTINCT STABBR FROM inst ORDER BY 1", conn).STABBR)
+           states.insert(0, '')
+           session['states'] = states
        return render_template('landing.html', user =username, user_id=user_id,
-                              text_default =text_default, default_selected=default_selected)
+                              text_default =text_default, default_selected=default_selected,
+                              states = states)
    else:
        print('Request for landing page received with no or invalid email -- redirecting')
        return redirect(url_for('index'))
@@ -43,18 +47,19 @@ def login():
 #     idx = inst_tbl.apply(lambda x: great_circle((x["LATITUDE"], x["LONGITUDE"]), (lat, lon)).miles, axis=1)
 #     return inst_tbl.loc[:, ['UNITID']].assign(DISTANCE_MI=idx)
 def update():
-    default_value = 'False'
+    text_default = 'False'
     user = session.get('user')
+    states = session.get('states')
     input_zip = request.form.get('zip')
     max_dist = request.form.get('zip_dist')
-    degree = request.form.get('degree')
-    default_selected = {'zip_pref': request.form.get('zip_pref'),
+    default_selected = {'states': request.form.getlist('states') if 'states' in request.form else [],
+                        'zip_pref': request.form.get('zip_pref'),
+                        'degree': request.form.get('degree'),
                         'degree_pref': request.form.get('degree_pref')}
 
-    print(input_zip, max_dist, degree)
-    return render_template('landing.html', user =user, default_value =default_value, default_selected= default_selected,
-                           zip =input_zip, zip_dist=max_dist,
-                           degree= degree)
+    print(input_zip, max_dist, default_selected['states'])
+    return render_template('landing.html', user =user, text_default =text_default, default_selected= default_selected,
+                           states=states, zip =input_zip, zip_dist=max_dist)
     # with sqlite3.connect(db_path) as conn:
     #     geo = pd.read_sql("SELECT * FROM geo", conn)
     #     inst = pd.read_sql("SELECT UNITID, LATITUDE, LONGITUDE FROM inst", conn)
